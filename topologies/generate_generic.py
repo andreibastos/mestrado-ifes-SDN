2ad8cca9d99c61f90c1901bd611f83e3d1358100
Andrei <andreibastos@outlook.com>
2020-08-06 18:05:27 -0700

    chore: geração de topologias
    - removido adicionar por número de portas
    - adicionado visualização
    - leitura e criação de topologia com base em arquivo
    - geração de topo{1,2,3}.txt

--- topologies/generate_generic.py
+++ topologies/generate_generic.py
@@ -2,25 +2,26 @@
 # -*- coding: utf-8 -*-
 
 """
-Esse é o que gera topologias aleatórias
+Esse programa gera topologias aleatórias
 """
 
 from networkx import nx
+import matplotlib.pyplot as plt
 import argparse
 from json import dump
 
 ## tratando argumentos 
 parser = argparse.ArgumentParser()
-parser.add_argument("-o","--output", help="Arquivo de saída quando topologia é generic", default='topo.txt')
-parser.add_argument("-k", "--ports", help="número de portas do switch", dest="k", type=int, default=4)
-parser.add_argument("-s", "--switches", help="número de portas do switch", dest="s", type=int)
-parser.add_argument("-l", "--links", help="número de portas do switch", dest="l", type=int)
+parser.add_argument("-s", "--switches", help="número de switches", dest="s", type=int, default=4)
+parser.add_argument("-l", "--links", help="número links na rede (default: dobro de switches)", dest="l", type=int)
+parser.add_argument("-o","--output", help="Arquivo de saída", default='topo.txt')
+parser.add_argument("-v", "--view", help="visualizar a rede", dest="view", action="store_true", default=False)
 args = parser.parse_args() # realiza o parse
 print args
-output = args.output # arquivo de saida 
-k = args.k # numero de portas dos switches
-switches = args.s if args.s else k*k  # é a quantidade de switches ou o quadrado de portas
+switches = args.s  # é a quantidade de switches
 links = args.l if args.l else 2*switches  # é quantidade de enlaces ou o dobro de switches
+output = args.output # arquivo de saida 
+view = args.view # indica se quer visualizar a rede
 
 ## manipulando o grafo
 G = nx.gnm_random_graph(switches, links) # grapho aleatório
@@ -36,6 +37,10 @@ for line in nx.generate_adjlist(G): # para cada linha das adjascentes
 ## manipulando arquivo
 with open(output, "w") as f: # abre o arquivo para escrita
     dump(edges, f) # escreve o arquivo
+
+if view:
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
 
 __author__ = 'Andrei Bastos'
 __email__ = 'andreibastos@outlook.com'
--- topologies/generic.py
+++ topologies/generic.py
@@ -15,18 +15,17 @@ class GenericTopo(Topo):
 
         with open(filename, 'r') as f:
             self.edges = load(f)
-            print(self.edges)
         
         for edge in self.edges:
-            switches = list()
+            switches = []
             for node in edge:
                 switch = 's%s'%(node+1)
+                switches.append(switch)
                 if not switch in self.switches():
                     self.addSwitch(switch)
                     host = self.addHost('h%s'%(node+1))
                     self.addLink(host, switch)
-                switches.append(switch)
-            self.addLink(switches[0], switches[1])
+            self.addLink(switches[0],switches[1])
 
 __author__ = 'Andrei Bastos'
 __email__ = 'andreibastos@outlook.com'
