bfd4491856882e2422424d34847713182164b57e
Andrei <andreibastos@outlook.com>
2020-08-26 22:51:57 -0700

    chore: teste completo

--- full_test.sh
+++ full_test.sh
@@ -7,57 +7,57 @@
 DIR_RESULTS=results
 
 mkdir $DIR_RESULTS -p
-# # fattre
-# # OSPF
-# sudo python main.py -t fattree -k 4 --method OSPF
-# sed -i 's/total/Fattree - OSPF/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/fattree_OSPF.bwm
+# fattre
+# OSPF
+sudo python main.py -t fattree -k 4 --method OSPF
+sed -i 's/total/Fattree - OSPF/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/fattree_OSPF.bwm
 
-# # # ECMP
-# sudo python main.py -t fattree -k 4 --method ECMP
-# sed -i 's/total/Fattree - ECMP/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/fattree_ECMP.bwm
-
-# cat $DIR_RESULTS/fattree_OSPF.bwm >$DIR_RESULTS/fattree.bwm
-# cat $DIR_RESULTS/fattree_ECMP.bwm >>$DIR_RESULTS/fattree.bwm
-
-# python plot_traffic.py -f $DIR_RESULTS/fattree.bwm -o $DIR_RESULTS/fattree.png
-
-# # genérica 1
-# python topologies/generate_generic.py -o topologies/topo1.txt --switches 16 --links 32
-
-# # OSPF
-# sudo python main.py -t generic -f topologies/topo1.txt --method OSPF
-# sed -i 's/total/Topologia Generica 1 - OSPF/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/topo1_OSPF.bwm
 # # ECMP
-# sudo python main.py -t generic -f topologies/topo1.txt --method ECMP
-# sed -i 's/total/Topologia Generica 1 - ECMP/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/topo1_ECMP.bwm
+sudo python main.py -t fattree -k 4 --method ECMP
+sed -i 's/total/Fattree - ECMP/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/fattree_ECMP.bwm
 
-# # genérica 2
-# python topologies/generate_generic.py -o topologies/topo2.txt --switches 16 --links 32
+cat $DIR_RESULTS/fattree_OSPF.bwm >$DIR_RESULTS/fattree.bwm
+cat $DIR_RESULTS/fattree_ECMP.bwm >>$DIR_RESULTS/fattree.bwm
+
+python plot_traffic.py -f $DIR_RESULTS/fattree.bwm -o $DIR_RESULTS/fattree.png
+
+# genérica 1
+python topologies/generate_generic.py -o topologies/topo1.txt --switches 16 --links 32
+
+# OSPF
+sudo python main.py -t generic -f topologies/topo1.txt --method OSPF
+sed -i 's/total/Topologia Generica 1 - OSPF/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/topo1_OSPF.bwm
+# ECMP
+sudo python main.py -t generic -f topologies/topo1.txt --method ECMP
+sed -i 's/total/Topologia Generica 1 - ECMP/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/topo1_ECMP.bwm
+
+# genérica 2
+python topologies/generate_generic.py -o topologies/topo2.txt --switches 16 --links 32
+
+# OSPF
+sudo python main.py -t generic -f topologies/topo2.txt --method OSPF
+sed -i 's/total/Topologia Generica 2 - OSPF/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/topo2_OSPF.bwm
+# ECMP
+sudo python main.py -t generic -f topologies/topo2.txt --method ECMP
+sed -i 's/total/Topologia Generica 2 - ECMP/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/topo2_ECMP.bwm
+
+# genérica 3
+python topologies/generate_generic.py -o topologies/topo3.txt --switches 16 --links 32
 
 # # OSPF
-# sudo python main.py -t generic -f topologies/topo2.txt --method OSPF
-# sed -i 's/total/Topologia Generica 2 - OSPF/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/topo2_OSPF.bwm
-# # ECMP
-# sudo python main.py -t generic -f topologies/topo2.txt --method ECMP
-# sed -i 's/total/Topologia Generica 2 - ECMP/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/topo2_ECMP.bwm
-
-# # genérica 3
-# python topologies/generate_generic.py -o topologies/topo3.txt --switches 16 --links 32
-
-# # # OSPF
-# sudo python main.py -t generic -f topologies/topo3.txt --method OSPF
-# sed -i 's/total/Topologia Generica 3 - OSPF/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/topo3_OSPF.bwm
-# # ECMP
-# sudo python main.py -t generic -f topologies/topo3.txt --method ECMP
-# sed -i 's/total/Topologia Generica 3 - ECMP/g' dados.bwm
-# cp dados.bwm $DIR_RESULTS/topo3_ECMP.bwm
+sudo python main.py -t generic -f topologies/topo3.txt --method OSPF
+sed -i 's/total/Topologia Generica 3 - OSPF/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/topo3_OSPF.bwm
+# ECMP
+sudo python main.py -t generic -f topologies/topo3.txt --method ECMP
+sed -i 's/total/Topologia Generica 3 - ECMP/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/topo3_ECMP.bwm
 
 # junta todos resultados em um arquivo unico
 cat $DIR_RESULTS/topo1_OSPF.bwm >$DIR_RESULTS/topo.bwm
@@ -68,8 +68,17 @@ cat $DIR_RESULTS/topo3_OSPF.bwm >>$DIR_RESULTS/topo.bwm
 cat $DIR_RESULTS/topo3_ECMP.bwm >>$DIR_RESULTS/topo.bwm
 python plot_traffic.py -f $DIR_RESULTS/topo.bwm -o $DIR_RESULTS/topo.png
 
-# ## bcube
-# # OSPF
-# sudo python main.py -t bcube -k 1 -n 4 --method OSPF
-# # ECMP
-# sudo python main.py -t bcube -k 1 -n 4 --method ECMP
+## bcube
+# OSPF
+sudo python main.py -t bcube -k 1 -n 4 --method OSPF
+sed -i 's/total/Bcube - OSPF/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/bcube_OSPF.bwm
+
+# ECMP
+sudo python main.py -t bcube -k 1 -n 4 --method ECMP
+sed -i 's/total/Bcube - ECMP/g' dados.bwm
+cp dados.bwm $DIR_RESULTS/bcube_ECMP.bwm
+
+cat $DIR_RESULTS/bcube_OSPF.bwm >$DIR_RESULTS/bcube.bwm
+cat $DIR_RESULTS/bcube_ECMP.bwm >>$DIR_RESULTS/bcube.bwm
+python plot_traffic.py -f $DIR_RESULTS/bcube.bwm -o $DIR_RESULTS/bcube.png
